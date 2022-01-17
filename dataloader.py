3b50705fc54eab9946ed8adc9e502d24bf3bc5ea
CejkaLuk <44003242+CejkaLuk@users.noreply.github.com>
2022-01-17 21:43:30 +0100

    Added loading data from file.

---
+++ dataloader.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+
+class DataLoader:
+
+    def __init__(self, file_path=None):
+        self.data = []
+
+        # In case the user wants to load the data using the function and not this constructor
+        if file_path is not None:
+            self.load_data_from_file(file_path)
+
+    def load_data_from_file(self, file_path):
+        """Loads coordinate data from a csv file."""
+        print(f'Loading data from {file_path}...')
+
+        if not os.path.exists(file_path):
+            raise AssertionError(f'File "{file_path}" not found!')
+
+        with open(file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+
+            # Skip header of csv file
+            next(csv_reader, None)
+
+            # Load every row
+            for row in csv_reader:
+                self.data.append((float(row[0]), float(row[1])))
+
+        print(f'Processed {len(self.data)} lines.')
